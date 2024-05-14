e03c8e61f36994b7c24d0c95a8c7885451bc94bf
eminano <esther@xata.io>
2024-05-14 16:24:33 +0200

    Add postgres impl of schemalog store

---
+++ pkg/schemalog/postgres/pg_schemalog_store.go
@@ -0,0 +1,95 @@
+package postgres
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/rs/zerolog/log"
+	"github.com/xataio/pgstream/pkg/schemalog"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+const (
+	schema         = "pgstream"
+	schemaLogTable = "schema_log"
+)
+
+// Store is a postgres implementation of the schemalog.Store interface
+type Store struct {
+	querier querier
+}
+
+type querier interface {
+	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
+	Exec(ctx context.Context, sql string, args ...any) (tag pgconn.CommandTag, err error)
+	Close(ctx context.Context)
+}
+
+func NewSchemalogStore(ctx context.Context, cfg *pgx.ConnConfig) (*Store, error) {
+	pgConn, err := pgx.ConnectConfig(ctx, cfg)
+	if err != nil {
+		return nil, fmt.Errorf("create postgres client: %w", err)
+	}
+	return &Store{
+		querier: &pgxConn{Conn: pgConn},
+	}, nil
+}
+
+func NewSchemalogStoreWithQuerier(querier querier) *Store {
+	return &Store{
+		querier: querier,
+	}
+}
+
+// Fetch retrieves the latest schema log entry for schemaName. If ackedOnly is set, the function returns the last acked
+// entry. If ackedOnly is NOT set, the function returns the latest entry no matter the acked flag.
+func (s *Store) Fetch(ctx context.Context, schemaName string, ackedOnly bool) (*schemalog.LogEntry, error) {
+	ackCondition := ""
+	if ackedOnly {
+		ackCondition = "and acked"
+	}
+	sql := fmt.Sprintf(`select id, version, schema_name, schema, created_at, acked from %s.%s where schema_name = $1 %s order by version desc limit 1`, schema, schemaLogTable, ackCondition)
+
+	l := &schemalog.LogEntry{}
+	if err := s.querier.QueryRow(ctx, sql, schemaName).Scan(&l.ID, &l.Version, &l.SchemaName, &l.Schema, &l.CreatedAt, &l.Acked); err != nil {
+		log.Ctx(ctx).Warn().Err(err).Msgf("error fetching schema log for schema %s", schemaName)
+		return nil, mapError(err)
+	}
+
+	return l, nil
+}
+
+func (s *Store) Ack(ctx context.Context, logEntry *schemalog.LogEntry) error {
+	sql := fmt.Sprintf(`update %s.%s set acked = true where id = $1 and schema_name = $2`, schema, schemaLogTable)
+	_, err := s.querier.Exec(ctx, sql, logEntry.ID.String(), logEntry.SchemaName)
+	if err != nil {
+		return mapError(fmt.Errorf("failed to ack schema log (%s) for schema (%s) version (%d): %w", logEntry.ID, logEntry.SchemaName, logEntry.Version, err))
+	}
+
+	return nil
+}
+
+func (s *Store) Close() error {
+	s.querier.Close(context.Background())
+	return nil
+}
+
+// pgxConn is a wrapper around the pgx.Conn to implement the same interface all
+// other pg packages use for pool/connections
+type pgxConn struct {
+	*pgx.Conn
+}
+
+func (c *pgxConn) Close(ctx context.Context) {
+	c.Conn.Close(ctx)
+}
+
+func mapError(err error) error {
+	if errors.Is(err, pgx.ErrNoRows) {
+		return schemalog.ErrNoRows
+	}
+	return err
+}
---
+++ pkg/schemalog/postgres/pg_schemalog_store_test.go
@@ -0,0 +1,182 @@
+package postgres
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/rs/xid"
+	"github.com/stretchr/testify/require"
+	"github.com/xataio/pgstream/pkg/schemalog"
+)
+
+func TestStore_Fetch(t *testing.T) {
+	t.Parallel()
+
+	testSchema := "test_schema"
+	testID := xid.New()
+	testLogEntry := &schemalog.LogEntry{
+		ID: testID,
+	}
+	errTest := errors.New("oh noes")
+
+	tests := []struct {
+		name      string
+		querier   querier
+		ackedOnly bool
+
+		wantLogEntry *schemalog.LogEntry
+		wantErr      error
+	}{
+		{
+			name: "ok - without acked",
+			querier: &mockQuerier{
+				queryRowFn: func(_ context.Context, query string, args ...any) pgx.Row {
+					require.Len(t, args, 1)
+					require.Equal(t, args[0], testSchema)
+					require.Equal(t,
+						fmt.Sprintf("select id, version, schema_name, schema, created_at, acked from %s.%s where schema_name = $1  order by version desc limit 1", schema, schemaLogTable),
+						query)
+					return &mockRow{logEntry: testLogEntry}
+				},
+			},
+
+			wantLogEntry: testLogEntry,
+			wantErr:      nil,
+		},
+		{
+			name: "ok - with acked",
+			querier: &mockQuerier{
+				queryRowFn: func(_ context.Context, query string, args ...any) pgx.Row {
+					require.Len(t, args, 1)
+					require.Equal(t, args[0], testSchema)
+					require.Equal(t,
+						fmt.Sprintf("select id, version, schema_name, schema, created_at, acked from %s.%s where schema_name = $1 and acked order by version desc limit 1", schema, schemaLogTable),
+						query)
+					return &mockRow{logEntry: testLogEntry}
+				},
+			},
+			ackedOnly: true,
+
+			wantLogEntry: testLogEntry,
+			wantErr:      nil,
+		},
+		{
+			name: "error - querying rows",
+			querier: &mockQuerier{
+				queryRowFn: func(context.Context, string, ...any) pgx.Row {
+					return &mockRow{scanFn: func(...any) error { return errTest }}
+				},
+			},
+
+			wantLogEntry: nil,
+			wantErr:      errTest,
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			s := NewSchemalogStoreWithQuerier(tc.querier)
+
+			logEntry, err := s.Fetch(context.Background(), testSchema, tc.ackedOnly)
+			require.ErrorIs(t, err, tc.wantErr)
+			require.Equal(t, tc.wantLogEntry, logEntry)
+		})
+	}
+}
+
+func TestStore_Ack(t *testing.T) {
+	t.Parallel()
+
+	testSchema := "test_schema"
+	testID := xid.New()
+	testLogEntry := &schemalog.LogEntry{
+		ID:         testID,
+		SchemaName: testSchema,
+	}
+	errTest := errors.New("oh noes")
+
+	tests := []struct {
+		name     string
+		querier  querier
+		logEntry *schemalog.LogEntry
+
+		wantErr error
+	}{
+		{
+			name: "ok",
+			querier: &mockQuerier{
+				execFn: func(_ context.Context, query string, args ...any) (pgconn.CommandTag, error) {
+					require.Len(t, args, 2)
+					require.Equal(t, args[0], testID.String())
+					require.Equal(t, args[1], testSchema)
+					require.Equal(t,
+						fmt.Sprintf(`update %s.%s set acked = true where id = $1 and schema_name = $2`, schema, schemaLogTable),
+						query)
+					return pgconn.NewCommandTag("1"), nil
+				},
+			},
+			logEntry: testLogEntry,
+
+			wantErr: nil,
+		},
+		{
+			name: "error - executing update query",
+			querier: &mockQuerier{
+				execFn: func(_ context.Context, query string, args ...any) (pgconn.CommandTag, error) {
+					return pgconn.NewCommandTag(""), errTest
+				},
+			},
+			logEntry: testLogEntry,
+
+			wantErr: errTest,
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			s := NewSchemalogStoreWithQuerier(tc.querier)
+
+			err := s.Ack(context.Background(), tc.logEntry)
+			require.ErrorIs(t, err, tc.wantErr)
+		})
+	}
+}
+
+func Test_mapError(t *testing.T) {
+	t.Parallel()
+
+	errTest := errors.New("oh noes")
+
+	tests := []struct {
+		err     error
+		wantErr error
+	}{
+		{
+			err:     errTest,
+			wantErr: errTest,
+		},
+		{
+			err:     fmt.Errorf("another error: %w", pgx.ErrNoRows),
+			wantErr: schemalog.ErrNoRows,
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.err.Error(), func(t *testing.T) {
+			t.Parallel()
+
+			require.ErrorIs(t, mapError(tc.err), tc.wantErr)
+		})
+	}
+}
