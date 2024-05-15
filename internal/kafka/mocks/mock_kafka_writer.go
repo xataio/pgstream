1862eeaeb2dc7c2341b88f58eaf8591f51074e13
eminano <esther@xata.io>
2024-05-15 16:24:05 +0200

    Add mock kafka writer

---
+++ internal/kafka/mocks/mock_kafka_writer.go
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package mocks
+
+import (
+	"context"
+	"sync/atomic"
+
+	"github.com/xataio/pgstream/internal/kafka"
+)
+
+type Writer struct {
+	WriteMessagesFn func(context.Context, uint64, ...kafka.Message) error
+	CloseFn         func() error
+	WriteCalls      uint64
+}
+
+func (m *Writer) WriteMessages(ctx context.Context, msgs ...kafka.Message) error {
+	atomic.AddUint64(&m.WriteCalls, 1)
+	return m.WriteMessagesFn(ctx, m.GetWriteCalls(), msgs...)
+}
+
+func (m *Writer) Close() error {
+	return m.CloseFn()
+}
+
+func (m *Writer) GetWriteCalls() uint64 {
+	return atomic.LoadUint64(&m.WriteCalls)
+}
