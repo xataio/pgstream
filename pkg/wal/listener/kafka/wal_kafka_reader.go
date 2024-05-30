df2bef350b9fbbd8cddfc18e1bf2131f865b953f
eminano <esther@xata.io>
2024-05-30 16:53:00 +0200

    Remove checkpoint logic from wal kafka reader

--- pkg/wal/listener/kafka/wal_kafka_reader.go
+++ pkg/wal/listener/kafka/wal_kafka_reader.go
@@ -8,7 +8,6 @@ import (
 	"errors"
 	"fmt"
 	"runtime/debug"
-	"time"
 
 	"github.com/xataio/pgstream/internal/backoff"
 	"github.com/xataio/pgstream/internal/kafka"
@@ -25,8 +24,6 @@ type Reader struct {
 
 	// processRecord is called for a new record.
 	processRecord payloadProcessor
-
-	backoffProvider backoff.Provider
 }
 
 type ReaderConfig struct {
@@ -36,7 +33,6 @@ type ReaderConfig struct {
 
 type kafkaReader interface {
 	FetchMessage(context.Context) (*kafka.Message, error)
-	CommitMessages(context.Context, ...*kafka.Message) error
 	Close() error
 }
 
@@ -53,10 +49,7 @@ func NewReader(config ReaderConfig,
 	return &Reader{
 		reader:        reader,
 		processRecord: processRecord,
-		backoffProvider: func(ctx context.Context) backoff.Backoff {
-			return backoff.NewExponentialBackoff(ctx, &config.CommitBackoff)
-		},
-		unmarshaler: json.Unmarshal,
+		unmarshaler:   json.Unmarshal,
 	}, nil
 }
 
@@ -111,36 +104,4 @@ func (r *Reader) Close() {
 			Bytes("stack_trace", debug.Stack()).
 			Msg("error closing connection to kafka")
 	}
-}
-
-func (r *Reader) Checkpoint(ctx context.Context, positions []wal.CommitPosition) error {
-	msgs := make([]*kafka.Message, 0, len(positions))
-	for _, pos := range positions {
-		msgs = append(msgs, pos.KafkaPos)
-	}
-
-	if err := r.commitMessagesWithRetry(ctx, msgs); err != nil {
-		return err
-	}
-
-	for _, msg := range msgs {
-		log.Trace().
-			Str("topic", msg.Topic).
-			Int("partition", msg.Partition).
-			Int64("offset", msg.Offset).
-			Msg("committed")
-	}
-
-	return nil
-}
-
-func (r *Reader) commitMessagesWithRetry(ctx context.Context, msgs []*kafka.Message) error {
-	bo := r.backoffProvider(ctx)
-	return bo.RetryNotify(
-		func() error {
-			return r.reader.CommitMessages(ctx, msgs...)
-		},
-		func(err error, d time.Duration) {
-			log.Warn().Err(err).Msgf("failed to commit messages. Retrying in %v", d)
-		})
 }
--- pkg/wal/listener/kafka/wal_kafka_reader_test.go
+++ pkg/wal/listener/kafka/wal_kafka_reader_test.go
@@ -11,8 +11,6 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
-	"github.com/xataio/pgstream/internal/backoff"
-	backoffmocks "github.com/xataio/pgstream/internal/backoff/mocks"
 	"github.com/xataio/pgstream/internal/kafka"
 	kafkamocks "github.com/xataio/pgstream/internal/kafka/mocks"
 	"github.com/xataio/pgstream/pkg/wal"
@@ -186,91 +184,6 @@ func TestReader_Listen(t *testing.T) {
 					return
 				}
 			}
-		})
-	}
-}
-
-func TestReader_Checkpoint(t *testing.T) {
-	t.Parallel()
-
-	testMsgs := []*kafka.Message{
-		{
-			Key:   []byte("test-key-1"),
-			Value: []byte("test-value-1"),
-		},
-		{
-			Key:   []byte("test-key-2"),
-			Value: []byte("test-value-2"),
-		},
-	}
-
-	testPositions := []wal.CommitPosition{
-		{KafkaPos: testMsgs[0]},
-		{KafkaPos: testMsgs[1]},
-	}
-
-	errTest := errors.New("oh noes")
-
-	tests := []struct {
-		name            string
-		reader          *kafkamocks.Reader
-		backoffProvider backoff.Provider
-
-		wantErr error
-	}{
-		{
-			name: "ok",
-			reader: &kafkamocks.Reader{
-				CommitMessagesFn: func(ctx context.Context, msgs ...*kafka.Message) error {
-					require.ElementsMatch(t, msgs, testMsgs)
-					return nil
-				},
-			},
-			backoffProvider: func(ctx context.Context) backoff.Backoff {
-				return &backoffmocks.Backoff{
-					RetryNotifyFn: func(o backoff.Operation, n backoff.Notify) error {
-						return o()
-					},
-				}
-			},
-
-			wantErr: nil,
-		},
-		{
-			name: "error - committing messages",
-			reader: &kafkamocks.Reader{
-				CommitMessagesFn: func(ctx context.Context, msgs ...*kafka.Message) error {
-					return errTest
-				},
-			},
-			backoffProvider: func(ctx context.Context) backoff.Backoff {
-				return &backoffmocks.Backoff{
-					RetryNotifyFn: func(o backoff.Operation, n backoff.Notify) error {
-						err := o()
-						if err != nil {
-							n(err, 50*time.Millisecond)
-						}
-						return err
-					},
-				}
-			},
-
-			wantErr: errTest,
-		},
-	}
-
-	for _, tc := range tests {
-		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
-
-			r := Reader{
-				reader:          tc.reader,
-				backoffProvider: tc.backoffProvider,
-			}
-
-			err := r.Checkpoint(context.Background(), testPositions)
-			require.ErrorIs(t, err, tc.wantErr)
 		})
 	}
 }
