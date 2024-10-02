1a988ca8e50819cd7602b426ad76a2dbe227ff23
eminano <esther@xata.io>
2024-10-02 13:20:24 +0200

    Add snapshot store

---
+++ pkg/snapshot/snapshot.go
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package snapshot
+
+type Snapshot struct {
+	SchemaName          string
+	TableName           string
+	IdentityColumnNames []string
+	Status              Status
+	Error               error
+}
+
+type Status string
+
+const (
+	StatusRequested = Status("requested")
+	StatusStarted   = Status("started")
+	StatusCompleted = Status("completed")
+)
+
+func (s *Snapshot) IsValid() bool {
+	return s != nil && s.SchemaName != "" && s.TableName != "" && len(s.IdentityColumnNames) > 0
+}
+
+func (s *Snapshot) MarkCompleted(err error) {
+	s.Status = StatusCompleted
+	s.Error = err
+}
+
+func (s *Snapshot) MarkStarted() {
+	s.Status = StatusStarted
+}
