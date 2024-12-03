cf1123299ffa1258c306f4607bf5cbecb906a5d4
eminano <esther@xata.io>
2024-12-03 17:38:52 +0100

    Add tx options to pg internal lib

--- internal/postgres/pg_tx.go
+++ internal/postgres/pg_tx.go
@@ -8,6 +8,27 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
+type TxIsolationLevel string
+
+const (
+	Serializable    TxIsolationLevel = "serializable"
+	RepeatableRead  TxIsolationLevel = "repeatable read"
+	ReadCommitted   TxIsolationLevel = "read committed"
+	ReadUncommitted TxIsolationLevel = "read uncommitted"
+)
+
+type TxAccessMode string
+
+const (
+	ReadWrite TxAccessMode = "read write"
+	ReadOnly  TxAccessMode = "read only"
+)
+
+type TxOptions struct {
+	IsolationLevel TxIsolationLevel
+	AccessMode     TxAccessMode
+}
+
 type Txn struct {
 	pgx.Tx
 }
@@ -25,4 +46,11 @@ func (t *Txn) Query(ctx context.Context, query string, args ...any) (Rows, error
 func (t *Txn) Exec(ctx context.Context, query string, args ...any) (CommandTag, error) {
 	tag, err := t.Tx.Exec(ctx, query, args...)
 	return CommandTag{tag}, mapError(err)
+}
+
+func toTxOptions(opts TxOptions) pgx.TxOptions {
+	return pgx.TxOptions{
+		IsoLevel:   pgx.TxIsoLevel(opts.IsolationLevel),
+		AccessMode: pgx.TxAccessMode(opts.AccessMode),
+	}
 }
