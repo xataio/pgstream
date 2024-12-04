c32b7d3705568916958e7f81bbc765445d62789c
eminano <esther@xata.io>
2024-12-04 16:31:45 +0100

    Add support to provide LSN

--- pkg/wal/replication/postgres/pg_replication_handler_test.go
+++ pkg/wal/replication/postgres/pg_replication_handler_test.go
@@ -18,8 +18,6 @@ import (
 func TestHandler_StartReplication(t *testing.T) {
 	t.Parallel()
 
-	defaultSlot := fmt.Sprintf("pgstream_%s_slot", testDBName)
-
 	tests := []struct {
 		name            string
 		replicationConn pgReplicationConn
@@ -109,57 +107,6 @@ func TestHandler_StartReplication(t *testing.T) {
 			slotName: testSlot,
 
 			wantErr: nil,
-		},
-		{
-			name: "ok - with default slot name",
-			replicationConn: &pgmocks.ReplicationConn{
-				IdentifySystemFn: func(ctx context.Context) (pglib.IdentifySystemResult, error) {
-					return pglib.IdentifySystemResult{
-						DBName:   testDBName,
-						SystemID: "tes-sys-id",
-					}, nil
-				},
-				StartReplicationFn: func(ctx context.Context, cfg pglib.ReplicationConfig) error {
-					require.Equal(t, testLSN, cfg.StartPos)
-					require.Equal(t, defaultSlot, cfg.SlotName)
-					require.Equal(t, pluginArguments, cfg.PluginArguments)
-					return nil
-				},
-				SendStandbyStatusUpdateFn: func(ctx context.Context, lsn uint64) error {
-					require.Equal(t, testLSN, lsn)
-					return nil
-				},
-			},
-			connBuilder: func() (pglib.Querier, error) {
-				return &pgmocks.Querier{
-					QueryRowFn: func(ctx context.Context, query string, args ...any) pglib.Row {
-						require.Len(t, args, 1)
-						require.Equal(t, args[0], defaultSlot)
-						switch query {
-						case "select restart_lsn from pg_replication_slots where slot_name=$1":
-							return &mockRow{scanFn: func(args ...any) error { return errors.New("restart lsn should not be called") }}
-						case "select confirmed_flush_lsn from pg_replication_slots where slot_name=$1":
-							return &mockRow{lsn: testLSNStr}
-						default:
-							return &mockRow{scanFn: func(args ...any) error { return fmt.Errorf("unexpected query: %s", query) }}
-						}
-					},
-					CloseFn: func(ctx context.Context) error { return nil },
-				}, nil
-			},
-
-			wantErr: nil,
-		},
-		{
-			name: "error - identifying system",
-			replicationConn: &pgmocks.ReplicationConn{
-				IdentifySystemFn: func(ctx context.Context) (pglib.IdentifySystemResult, error) {
-					return pglib.IdentifySystemResult{}, errTest
-				},
-			},
-			slotName: testSlot,
-
-			wantErr: errTest,
 		},
 		{
 			name: "error - creating connection",
@@ -476,6 +423,81 @@ func TestHandler_GetReplicationLag(t *testing.T) {
 			lag, err := h.GetReplicationLag(context.Background())
 			require.ErrorIs(t, err, tc.wantErr)
 			require.Equal(t, tc.wantLag, lag)
+		})
+	}
+}
+
+func TestHandler_GetCurrentLSN(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name        string
+		connBuilder func() (pglib.Querier, error)
+
+		wantLSN replication.LSN
+		wantErr error
+	}{
+		{
+			name: "ok",
+			connBuilder: func() (pglib.Querier, error) {
+				return &pgmocks.Querier{
+					QueryRowFn: func(ctx context.Context, query string, args ...any) pglib.Row {
+						require.Len(t, args, 1)
+						require.Equal(t, args[0], testSlot)
+						switch query {
+						case "SELECT confirmed_flush_lsn FROM pg_replication_slots WHERE slot_name=$1":
+							return &mockRow{lsn: testLSNStr}
+						default:
+							return &mockRow{scanFn: func(args ...any) error { return fmt.Errorf("unexpected query: %s", query) }}
+						}
+					},
+					CloseFn: func(ctx context.Context) error { return nil },
+				}, nil
+			},
+
+			wantLSN: replication.LSN(testLSN),
+			wantErr: nil,
+		},
+		{
+			name: "error - building connection",
+			connBuilder: func() (pglib.Querier, error) {
+				return nil, errTest
+			},
+
+			wantLSN: replication.LSN(0),
+			wantErr: errTest,
+		},
+		{
+			name: "error - getting current lsn",
+			connBuilder: func() (pglib.Querier, error) {
+				return &pgmocks.Querier{
+					QueryRowFn: func(ctx context.Context, query string, args ...any) pglib.Row {
+						return &mockRow{scanFn: func(args ...any) error { return errTest }}
+					},
+					CloseFn: func(ctx context.Context) error { return nil },
+				}, nil
+			},
+
+			wantLSN: replication.LSN(0),
+			wantErr: errTest,
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			h := Handler{
+				logger:                log.NewNoopLogger(),
+				pgConnBuilder:         tc.connBuilder,
+				pgReplicationSlotName: testSlot,
+				lsnParser:             NewLSNParser(),
+			}
+
+			lsn, err := h.GetCurrentLSN(context.Background())
+			require.ErrorIs(t, err, tc.wantErr)
+			require.Equal(t, tc.wantLSN, lsn)
 		})
 	}
 }
