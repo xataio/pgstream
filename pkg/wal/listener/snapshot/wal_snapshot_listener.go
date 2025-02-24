f0373f12215d7f33aa7f8a19bb603de6f42c1568
eminano <esther@xata.io>
2025-02-24 16:48:08 +0100

    Add snapshot listener

--- pkg/wal/listener/snapshot/wal_snapshot_listener.go
+++ pkg/wal/listener/snapshot/wal_snapshot_listener.go
@@ -2,9 +2,31 @@
 
 package snapshot
 
-import "context"
+import (
+	"context"
+)
 
 type Generator interface {
 	CreateSnapshot(context.Context) error
 	Close() error
+}
+
+type Listener struct {
+	generator Generator
+}
+
+func New(generator Generator) *Listener {
+	return &Listener{
+		generator: generator,
+	}
+}
+
+// Listen starts the snapshot generation process.
+func (l *Listener) Listen(ctx context.Context) error {
+	return l.generator.CreateSnapshot(ctx)
+}
+
+// Close closes the listener internal resources
+func (l *Listener) Close() error {
+	return l.generator.Close()
 }
