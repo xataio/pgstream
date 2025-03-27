52481e358b3ad590c9ba8659a824dc80008096d9
eminano <esther@xata.io>
2025-03-27 18:10:18 +0100

    Fix staticcheck lint

--- pkg/snapshot/generator/postgres/data/helper_test.go
+++ pkg/snapshot/generator/postgres/data/helper_test.go
@@ -4,15 +4,10 @@ package postgres
 
 import (
 	"context"
-	"fmt"
 	"sync"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/require"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/modules/postgres"
-	"github.com/testcontainers/testcontainers-go/wait"
 
 	pglib "github.com/xataio/pgstream/internal/postgres"
 	"github.com/xataio/pgstream/pkg/snapshot"
@@ -30,32 +25,6 @@ func (mp *mockRowProcessor) process(ctx context.Context, row *snapshot.Row) erro
 
 func (mp *mockRowProcessor) close() {
 	mp.once.Do(func() { close(mp.rowChan) })
-}
-
-type cleanup func() error
-
-func setupPostgresContainer(ctx context.Context) (cleanup, string, error) {
-	waitForLogs := wait.
-		ForLog("database system is ready to accept connections").
-		WithOccurrence(2).
-		WithStartupTimeout(5 * time.Second)
-
-	ctr, err := postgres.RunContainer(ctx,
-		testcontainers.WithImage("debezium/postgres:14-alpine"),
-		testcontainers.WithWaitStrategy(waitForLogs),
-	)
-	if err != nil {
-		return nil, "", fmt.Errorf("failed to start postgres container: %w", err)
-	}
-
-	pgurl, err := ctr.ConnectionString(ctx, "sslmode=disable")
-	if err != nil {
-		return nil, "", fmt.Errorf("retrieving connection string for postgres container: %w", err)
-	}
-
-	return func() error {
-		return ctr.Terminate(ctx)
-	}, pgurl, nil
 }
 
 func execQuery(t *testing.T, ctx context.Context, pgurl, query string) {
