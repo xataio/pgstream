7a311402476a9fa67634c8796ab60bd695309b13
Ahmet Gedemenli <afgedemenli@gmail.com>
2025-03-27 19:49:32 +0300

    Catch value too long error

--- pkg/wal/processor/postgres/postgres_batch_writer.go
+++ pkg/wal/processor/postgres/postgres_batch_writer.go
@@ -247,10 +247,12 @@ func (w *BatchWriter) isInternalError(err error) bool {
 	var errRelationDoesNotExist *pglib.ErrRelationDoesNotExist
 	var errConstraintViolation *pglib.ErrConstraintViolation
 	var errSyntaxError *pglib.ErrSyntaxError
+	var errStrValueTooLong *pglib.ErrStrValueTooLong
 	switch {
 	case errors.As(err, &errRelationDoesNotExist),
 		errors.As(err, &errConstraintViolation),
-		errors.As(err, &errSyntaxError):
+		errors.As(err, &errSyntaxError),
+		errors.As(err, &errStrValueTooLong):
 		return false
 	default:
 		return true
