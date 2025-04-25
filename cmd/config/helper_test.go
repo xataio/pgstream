941e22d87a9362ac95c2a092f4b106fa67646fcd
eminano <esther@xata.io>
2025-04-25 13:26:59 +0200

    Update config tests to use upper case table

--- cmd/config/helper_test.go
+++ cmd/config/helper_test.go
@@ -18,7 +18,7 @@ func validateTestStreamConfig(t *testing.T, streamConfig *stream.Config) {
 	assert.Equal(t, "postgresql://user:password@localhost:5432/mydatabase", streamConfig.Listener.Postgres.Replication.PostgresURL)
 	assert.Equal(t, "pgstream_mydatabase_slot", streamConfig.Listener.Postgres.Replication.ReplicationSlotName)
 	assert.NotNil(t, streamConfig.Listener.Postgres.Snapshot)
-	assert.ElementsMatch(t, []string{"test", "test_schema.test", "another_schema.*"}, streamConfig.Listener.Postgres.Snapshot.Adapter.Tables)
+	assert.ElementsMatch(t, []string{"test", "test_schema.Test", "another_schema.*"}, streamConfig.Listener.Postgres.Snapshot.Adapter.Tables)
 	assert.Equal(t, uint(4), streamConfig.Listener.Postgres.Snapshot.Adapter.SnapshotWorkers)
 	assert.Equal(t, uint(4), streamConfig.Listener.Postgres.Snapshot.Generator.SchemaWorkers)
 	assert.Equal(t, uint(4), streamConfig.Listener.Postgres.Snapshot.Generator.TableWorkers)
