091b500b05e03d0a4bde0c897c7105f72d6d883d
eminano <esther@xata.io>
2025-05-08 14:40:30 +0200

    Add otel config to CLI

--- cmd/config/config_yaml_test.go
+++ cmd/config/config_yaml_test.go
@@ -4,9 +4,11 @@ package config
 
 import (
 	"testing"
+	"time"
 
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/require"
+	"github.com/xataio/pgstream/pkg/otel"
 )
 
 func TestYAMLConfig_toStreamConfig(t *testing.T) {
@@ -159,7 +161,66 @@ func TestYAMLConfig_toStreamConfig_ErrorCases(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
 			_, err := tt.config.toStreamConfig()
+			require.ErrorIs(t, err, tt.wantErr)
+		})
+	}
+}
+
+func TestInstrumentationConfig_toOtelConfig(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name   string
+		config InstrumentationConfig
+
+		wantConfig *otel.Config
+		wantErr    error
+	}{
+		{
+			name: "valid config",
+			config: InstrumentationConfig{
+				Metrics: &MetricsConfig{
+					Endpoint:           "http://localhost:8080/metrics",
+					CollectionInterval: 10,
+				},
+				Traces: &TracesConfig{
+					Endpoint:    "http://localhost:8080/traces",
+					SampleRatio: 0.5,
+				},
+			},
+			wantConfig: &otel.Config{
+				Metrics: &otel.MetricsConfig{
+					Endpoint:           "http://localhost:8080/metrics",
+					CollectionInterval: time.Second * 10,
+				},
+				Traces: &otel.TracesConfig{
+					Endpoint:    "http://localhost:8080/traces",
+					SampleRatio: 0.5,
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "err - invalid trace sample ratio",
+			config: InstrumentationConfig{
+				Traces: &TracesConfig{
+					SampleRatio: 1.5,
+				},
+			},
+			wantConfig: nil,
+			wantErr:    errInvalidSampleRatio,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			cfg, err := tt.config.toOtelConfig()
+			require.Equal(t, tt.wantConfig, cfg)
 			require.ErrorIs(t, err, tt.wantErr)
 		})
 	}
