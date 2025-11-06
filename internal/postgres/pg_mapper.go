9b94b3c648eff6c7ef91a9ca86bece55ae4a655b
eminano <esther@xata.io>
2025-11-06 12:48:49 +0100

    Add cache for postgres mapper custom types

--- internal/postgres/pg_mapper.go
+++ internal/postgres/pg_mapper.go
@@ -6,18 +6,22 @@ import (
 	"context"
 	"fmt"
 
+	synclib "github.com/xataio/pgstream/internal/sync"
+
 	"github.com/jackc/pgx/v5/pgtype"
 )
 
 type Mapper struct {
-	querier Querier
-	pgMap   *pgtype.Map
+	querier      Querier
+	pgMap        *pgtype.Map
+	customOIDMap *synclib.Map[uint32, string]
 }
 
 func NewMapper(conn Querier) *Mapper {
 	return &Mapper{
-		querier: conn,
-		pgMap:   pgtype.NewMap(),
+		querier:      conn,
+		pgMap:        pgtype.NewMap(),
+		customOIDMap: synclib.NewMap[uint32, string](),
 	}
 }
 
@@ -30,9 +34,15 @@ func (m *Mapper) TypeForOID(ctx context.Context, oid uint32) (string, error) {
 }
 
 func (m *Mapper) queryType(ctx context.Context, oid uint32) (string, error) {
+	if customType, found := m.customOIDMap.Get(oid); found {
+		return customType, nil
+	}
+
 	var dataType string
 	if err := m.querier.QueryRow(ctx, "SELECT typname FROM pg_type WHERE oid = $1", oid).Scan(&dataType); err != nil {
 		return "unknown", fmt.Errorf("selecting type for OID %d: %w", oid, err)
 	}
+
+	m.customOIDMap.Set(oid, dataType)
 	return dataType, nil
 }
