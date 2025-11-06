f3ec37cf5e7f4b12c6069f67d22962ecc139c440
eminano <esther@xata.io>
2025-11-06 16:16:24 +0100

    Add postgres querier retrier

---
+++ internal/postgres/retrier/pg_querier_retrier.go
@@ -0,0 +1,157 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package retrier
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/xataio/pgstream/internal/postgres"
+	"github.com/xataio/pgstream/pkg/backoff"
+	loglib "github.com/xataio/pgstream/pkg/log"
+)
+
+type Querier struct {
+	connBuilder     connBuilder
+	querier         postgres.Querier
+	backoffProvider backoff.Provider
+	logger          loglib.Logger
+}
+
+type connBuilder func(context.Context) (postgres.Querier, error)
+
+func NewQuerier(ctx context.Context, cfg backoff.Config, connBuilder connBuilder, logger loglib.Logger) (*Querier, error) {
+	conn, err := connBuilder(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Querier{
+		connBuilder:     connBuilder,
+		querier:         conn,
+		backoffProvider: backoff.NewProvider(&cfg),
+		logger:          logger,
+	}, nil
+}
+
+func (q *Querier) Query(ctx context.Context, query string, args ...any) (postgres.Rows, error) {
+	var rows postgres.Rows
+	var err error
+	op := func() error {
+		rows, err = q.querier.Query(ctx, query, args...)
+		return err
+	}
+
+	if err := q.withRetry(ctx, op); err != nil {
+		return nil, err
+	}
+	return rows, nil
+}
+
+func (q *Querier) QueryRow(ctx context.Context, dest []any, query string, args ...any) error {
+	return q.withRetry(ctx, func() error {
+		return q.querier.QueryRow(ctx, dest, query, args...)
+	})
+}
+
+func (q *Querier) Exec(ctx context.Context, query string, args ...any) (postgres.CommandTag, error) {
+	var cmdTag postgres.CommandTag
+	var err error
+	op := func() error {
+		cmdTag, err = q.querier.Exec(ctx, query, args...)
+		return err
+	}
+
+	if err := q.withRetry(ctx, op); err != nil {
+		return postgres.CommandTag{}, err
+	}
+	return cmdTag, nil
+}
+
+func (q *Querier) ExecInTx(ctx context.Context, fn func(tx postgres.Tx) error) error {
+	return q.withRetry(ctx, func() error {
+		return q.querier.ExecInTx(ctx, fn)
+	})
+}
+
+func (q *Querier) ExecInTxWithOptions(ctx context.Context, fn func(tx postgres.Tx) error, txOpts postgres.TxOptions) error {
+	return q.withRetry(ctx, func() error {
+		return q.querier.ExecInTxWithOptions(ctx, fn, txOpts)
+	})
+}
+
+func (q *Querier) CopyFrom(ctx context.Context, tableName string, columnNames []string, srcRows [][]any) (int64, error) {
+	var copyCount int64
+	var err error
+	op := func() error {
+		copyCount, err = q.querier.CopyFrom(ctx, tableName, columnNames, srcRows)
+		return err
+	}
+
+	if err := q.withRetry(ctx, op); err != nil {
+		return 0, err
+	}
+	return copyCount, nil
+}
+
+func (q *Querier) Ping(ctx context.Context) error {
+	return q.querier.Ping(ctx)
+}
+
+func (q *Querier) Close(ctx context.Context) error {
+	return q.querier.Close(ctx)
+}
+
+func (q *Querier) withRetry(ctx context.Context, operation func() error) error {
+	err := operation()
+	if err == nil || !q.isRetriableError(err) {
+		return err
+	}
+
+	// only initialise the backoff provider if the operation fails
+	bo := q.backoffProvider(ctx)
+	err = bo.RetryNotify(func() error {
+		err = operation()
+		if err == nil {
+			return nil
+		}
+
+		if !q.isRetriableError(err) {
+			return fmt.Errorf("%w: %w", err, backoff.ErrPermanent)
+		}
+
+		if connErr := q.resetConn(ctx); connErr != nil {
+			return fmt.Errorf("unable to reset connection: %w", connErr)
+		}
+
+		return err
+	}, func(err error, d time.Duration) {
+		q.logger.Warn(err, "retrying Postgres operation after error", loglib.Fields{
+			"retry_delay": d.String(),
+		})
+	})
+
+	if err == nil {
+		q.logger.Info("retried Postgres operation succeeded")
+	}
+	return err
+}
+
+func (q *Querier) resetConn(ctx context.Context) error {
+	conn, connErr := q.connBuilder(ctx)
+	if connErr != nil {
+		return connErr
+	}
+	if q.querier != nil {
+		q.querier.Close(ctx)
+	}
+	q.querier = conn
+	return nil
+}
+
+func (q *Querier) isRetriableError(err error) bool {
+	// for now retry errors that are not context cancellation
+	return !errors.Is(err, context.Canceled)
+}
---
+++ internal/postgres/retrier/pg_querier_retrier_test.go
@@ -0,0 +1,531 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package retrier
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/stretchr/testify/require"
+	"github.com/xataio/pgstream/internal/postgres"
+	"github.com/xataio/pgstream/internal/postgres/mocks"
+	"github.com/xataio/pgstream/pkg/backoff"
+	loglib "github.com/xataio/pgstream/pkg/log"
+)
+
+func TestQuerier_Query(t *testing.T) {
+	t.Parallel()
+
+	retriableErr := errors.New("connection failed")
+	nonRetriableErr := context.Canceled
+
+	tests := []struct {
+		name    string
+		querier postgres.Querier
+
+		wantRowsFieldDescriptions []pgconn.FieldDescription
+		wantErr                   error
+	}{
+		{
+			name: "ok",
+			querier: &mocks.Querier{
+				QueryFn: func(_ context.Context, _ string, _ ...any) (postgres.Rows, error) {
+					return &mocks.Rows{
+						FieldDescriptionsFn: func() []pgconn.FieldDescription {
+							return []pgconn.FieldDescription{
+								{Name: "id"}, {Name: "name"},
+							}
+						},
+					}, nil
+				},
+			},
+			wantRowsFieldDescriptions: []pgconn.FieldDescription{
+				{Name: "id"}, {Name: "name"},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "error - non-retriable",
+			querier: &mocks.Querier{
+				QueryFn: func(_ context.Context, _ string, _ ...any) (postgres.Rows, error) {
+					return nil, nonRetriableErr
+				},
+			},
+			wantRowsFieldDescriptions: nil,
+			wantErr:                   nonRetriableErr,
+		},
+		{
+			name: "error - retriable but always fails",
+			querier: &mocks.Querier{
+				QueryFn: func(_ context.Context, _ string, _ ...any) (postgres.Rows, error) {
+					return nil, retriableErr
+				},
+			},
+			wantRowsFieldDescriptions: nil,
+			wantErr:                   retriableErr,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			q := &Querier{
+				connBuilder: func(ctx context.Context) (postgres.Querier, error) {
+					return tc.querier, nil
+				},
+				querier:         tc.querier,
+				backoffProvider: newMockBackoffProvider(),
+				logger:          loglib.NewNoopLogger(),
+			}
+
+			gotRows, gotErr := q.Query(context.Background(), "SELECT 1")
+			require.ErrorIs(t, gotErr, tc.wantErr)
+			if tc.wantRowsFieldDescriptions != nil {
+				require.NotNil(t, gotRows)
+				require.Equal(t, tc.wantRowsFieldDescriptions, gotRows.FieldDescriptions())
+			}
+		})
+	}
+}
+
+func TestQuerier_QueryRow(t *testing.T) {
+	t.Parallel()
+
+	retriableErr := errors.New("connection failed")
+	nonRetriableErr := context.Canceled
+
+	tests := []struct {
+		name    string
+		querier postgres.Querier
+		wantErr error
+	}{
+		{
+			name: "ok",
+			querier: &mocks.Querier{
+				QueryRowFn: func(_ context.Context, _ []any, _ string, _ ...any) error {
+					return nil
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "error - non-retriable",
+			querier: &mocks.Querier{
+				QueryRowFn: func(_ context.Context, _ []any, _ string, _ ...any) error {
+					return nonRetriableErr
+				},
+			},
+			wantErr: nonRetriableErr,
+		},
+		{
+			name: "error - retriable but always fails",
+			querier: &mocks.Querier{
+				QueryRowFn: func(_ context.Context, _ []any, _ string, _ ...any) error {
+					return retriableErr
+				},
+			},
+			wantErr: retriableErr,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			q := &Querier{
+				connBuilder: func(ctx context.Context) (postgres.Querier, error) {
+					return tc.querier, nil
+				},
+				querier:         tc.querier,
+				backoffProvider: newMockBackoffProvider(),
+				logger:          loglib.NewNoopLogger(),
+			}
+
+			gotErr := q.QueryRow(context.Background(), []any{}, "SELECT 1")
+			require.ErrorIs(t, gotErr, tc.wantErr)
+		})
+	}
+}
+
+func TestQuerier_Exec(t *testing.T) {
+	t.Parallel()
+
+	retriableErr := errors.New("connection failed")
+	nonRetriableErr := context.Canceled
+
+	testCommandTag := postgres.CommandTag{CommandTag: pgconn.NewCommandTag("UPDATE 1")}
+
+	tests := []struct {
+		name       string
+		querier    postgres.Querier
+		wantCmdTag postgres.CommandTag
+		wantErr    error
+	}{
+		{
+			name: "ok",
+			querier: &mocks.Querier{
+				ExecFn: func(_ context.Context, _ uint, _ string, _ ...any) (postgres.CommandTag, error) {
+					return testCommandTag, nil
+				},
+			},
+			wantCmdTag: testCommandTag,
+			wantErr:    nil,
+		},
+		{
+			name: "error - non-retriable",
+			querier: &mocks.Querier{
+				ExecFn: func(_ context.Context, _ uint, _ string, _ ...any) (postgres.CommandTag, error) {
+					return postgres.CommandTag{}, nonRetriableErr
+				},
+			},
+			wantCmdTag: postgres.CommandTag{},
+			wantErr:    nonRetriableErr,
+		},
+		{
+			name: "error - retriable but always fails",
+			querier: &mocks.Querier{
+				ExecFn: func(_ context.Context, _ uint, _ string, _ ...any) (postgres.CommandTag, error) {
+					return postgres.CommandTag{}, retriableErr
+				},
+			},
+			wantCmdTag: postgres.CommandTag{},
+			wantErr:    retriableErr,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			q := &Querier{
+				connBuilder: func(ctx context.Context) (postgres.Querier, error) {
+					return tc.querier, nil
+				},
+				querier:         tc.querier,
+				backoffProvider: newMockBackoffProvider(),
+				logger:          loglib.NewNoopLogger(),
+			}
+
+			gotCmdTag, gotErr := q.Exec(context.Background(), "UPDATE test SET id = 1")
+			require.ErrorIs(t, gotErr, tc.wantErr)
+			require.Equal(t, tc.wantCmdTag, gotCmdTag)
+		})
+	}
+}
+
+func TestQuerier_ExecInTx(t *testing.T) {
+	t.Parallel()
+
+	retriableErr := errors.New("connection failed")
+	nonRetriableErr := context.Canceled
+
+	tests := []struct {
+		name    string
+		querier postgres.Querier
+		wantErr error
+	}{
+		{
+			name: "ok",
+			querier: &mocks.Querier{
+				ExecInTxFn: func(_ context.Context, _ func(tx postgres.Tx) error) error {
+					return nil
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "error - non-retriable",
+			querier: &mocks.Querier{
+				ExecInTxFn: func(_ context.Context, _ func(tx postgres.Tx) error) error {
+					return nonRetriableErr
+				},
+			},
+			wantErr: nonRetriableErr,
+		},
+		{
+			name: "error - retriable but always fails",
+			querier: &mocks.Querier{
+				ExecInTxFn: func(_ context.Context, _ func(tx postgres.Tx) error) error {
+					return retriableErr
+				},
+			},
+			wantErr: retriableErr,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			q := &Querier{
+				connBuilder: func(ctx context.Context) (postgres.Querier, error) {
+					return tc.querier, nil
+				},
+				querier:         tc.querier,
+				backoffProvider: newMockBackoffProvider(),
+				logger:          loglib.NewNoopLogger(),
+			}
+
+			gotErr := q.ExecInTx(context.Background(), func(tx postgres.Tx) error {
+				return nil
+			})
+			require.ErrorIs(t, gotErr, tc.wantErr)
+		})
+	}
+}
+
+func TestQuerier_ExecInTxWithOptions(t *testing.T) {
+	t.Parallel()
+
+	retriableErr := errors.New("connection failed")
+	nonRetriableErr := context.Canceled
+
+	tests := []struct {
+		name    string
+		querier postgres.Querier
+		wantErr error
+	}{
+		{
+			name: "ok",
+			querier: &mocks.Querier{
+				ExecInTxWithOptionsFn: func(_ context.Context, _ uint, _ func(tx postgres.Tx) error, _ postgres.TxOptions) error {
+					return nil
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "error - non-retriable",
+			querier: &mocks.Querier{
+				ExecInTxWithOptionsFn: func(_ context.Context, _ uint, _ func(tx postgres.Tx) error, _ postgres.TxOptions) error {
+					return nonRetriableErr
+				},
+			},
+			wantErr: nonRetriableErr,
+		},
+		{
+			name: "error - retriable but always fails",
+			querier: &mocks.Querier{
+				ExecInTxWithOptionsFn: func(_ context.Context, _ uint, _ func(tx postgres.Tx) error, _ postgres.TxOptions) error {
+					return retriableErr
+				},
+			},
+			wantErr: retriableErr,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			q := &Querier{
+				connBuilder: func(ctx context.Context) (postgres.Querier, error) {
+					return tc.querier, nil
+				},
+				querier:         tc.querier,
+				backoffProvider: newMockBackoffProvider(),
+				logger:          loglib.NewNoopLogger(),
+			}
+
+			gotErr := q.ExecInTxWithOptions(context.Background(), func(tx postgres.Tx) error {
+				return nil
+			}, postgres.TxOptions{})
+			require.ErrorIs(t, gotErr, tc.wantErr)
+		})
+	}
+}
+
+func TestQuerier_CopyFrom(t *testing.T) {
+	t.Parallel()
+
+	retriableErr := errors.New("connection failed")
+	nonRetriableErr := context.Canceled
+
+	tests := []struct {
+		name      string
+		querier   postgres.Querier
+		wantCount int64
+		wantErr   error
+	}{
+		{
+			name: "ok",
+			querier: &mocks.Querier{
+				CopyFromFn: func(_ context.Context, _ string, _ []string, _ [][]any) (int64, error) {
+					return 5, nil
+				},
+			},
+			wantCount: 5,
+			wantErr:   nil,
+		},
+		{
+			name: "error - non-retriable",
+			querier: &mocks.Querier{
+				CopyFromFn: func(_ context.Context, _ string, _ []string, _ [][]any) (int64, error) {
+					return 0, nonRetriableErr
+				},
+			},
+			wantCount: 0,
+			wantErr:   nonRetriableErr,
+		},
+		{
+			name: "error - retriable but always fails",
+			querier: &mocks.Querier{
+				CopyFromFn: func(_ context.Context, _ string, _ []string, _ [][]any) (int64, error) {
+					return 0, retriableErr
+				},
+			},
+			wantCount: 0,
+			wantErr:   retriableErr,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			q := &Querier{
+				connBuilder: func(ctx context.Context) (postgres.Querier, error) {
+					return tc.querier, nil
+				},
+				querier:         tc.querier,
+				backoffProvider: newMockBackoffProvider(),
+				logger:          loglib.NewNoopLogger(),
+			}
+
+			gotCount, gotErr := q.CopyFrom(context.Background(), "test_table", []string{"id", "name"}, [][]any{{1, "test"}})
+			require.ErrorIs(t, gotErr, tc.wantErr)
+			require.Equal(t, tc.wantCount, gotCount)
+		})
+	}
+}
+
+func TestQuerier_withRetry(t *testing.T) {
+	t.Parallel()
+
+	nonRetriableErr := context.Canceled
+	retriableErr := errors.New("retriable error")
+	connBuilderErr := errors.New("connection builder error")
+
+	tests := []struct {
+		name            string
+		connBuilder     func(ctx context.Context) (postgres.Querier, error)
+		backoffProvider backoff.Provider
+		op              func(i uint) error
+
+		wantErr error
+	}{
+		{
+			name: "ok",
+			op: func(_ uint) error {
+				return nil
+			},
+
+			wantErr: nil,
+		},
+		{
+			name: "non retriable error",
+			op: func(_ uint) error {
+				return nonRetriableErr
+			},
+
+			wantErr: nonRetriableErr,
+		},
+		{
+			name: "retriable error and then succeeds",
+			connBuilder: func(ctx context.Context) (postgres.Querier, error) {
+				return &mocks.Querier{}, nil
+			},
+			backoffProvider: newMockBackoffProvider(),
+			op: func(i uint) error {
+				if i < 3 {
+					return retriableErr
+				}
+				return nil
+			},
+
+			wantErr: nil,
+		},
+		{
+			name: "retriable error but always fails",
+			connBuilder: func(ctx context.Context) (postgres.Querier, error) {
+				return &mocks.Querier{}, nil
+			},
+			backoffProvider: newMockBackoffProvider(),
+			op: func(_ uint) error {
+				return retriableErr
+			},
+
+			wantErr: retriableErr,
+		},
+		{
+			name: "non-retriable error after some retriable errors",
+			connBuilder: func(ctx context.Context) (postgres.Querier, error) {
+				return &mocks.Querier{}, nil
+			},
+			backoffProvider: newMockBackoffProvider(),
+			op: func(i uint) error {
+				if i < 3 {
+					return retriableErr
+				}
+				return nonRetriableErr
+			},
+
+			wantErr: nonRetriableErr,
+		},
+		{
+			name: "error resetting connection after retriable errors",
+			connBuilder: func(ctx context.Context) (postgres.Querier, error) {
+				return nil, connBuilderErr
+			},
+			backoffProvider: newMockBackoffProvider(),
+			op: func(i uint) error {
+				return retriableErr
+			},
+
+			wantErr: connBuilderErr,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			q := &Querier{
+				connBuilder:     tc.connBuilder,
+				backoffProvider: tc.backoffProvider,
+				logger:          loglib.NewNoopLogger(),
+			}
+
+			mockOp := mockOperation{
+				op: tc.op,
+			}
+
+			gotErr := q.withRetry(context.Background(), mockOp.Do)
+			require.ErrorIs(t, gotErr, tc.wantErr)
+		})
+	}
+}
+
+// mock backoff provider runs the operation for up to 3 times until it succeeds
+// or returns error
+func newMockBackoffProvider() backoff.Provider {
+	return func(ctx context.Context) backoff.Backoff {
+		return backoff.NewConstantBackoff(ctx, &backoff.ConstantConfig{
+			Interval:   0,
+			MaxRetries: 3,
+		})
+	}
+}
+
+type mockOperation struct {
+	calls uint
+	op    func(i uint) error
+}
+
+func (m *mockOperation) Do() error {
+	m.calls++
+	return m.op(m.calls)
+}
