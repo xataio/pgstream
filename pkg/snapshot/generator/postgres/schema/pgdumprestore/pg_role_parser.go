a7fd8b4a34230e98c1f971e5762c4d330bcf07b5
eminano <esther@xata.io>
2025-11-12 17:13:44 +0100

    Move role into separate file

--- pkg/snapshot/generator/postgres/schema/pgdumprestore/pg_role_parser.go
+++ pkg/snapshot/generator/postgres/schema/pgdumprestore/pg_role_parser.go
@@ -10,14 +10,6 @@ import (
 
 type roleSQLParser struct{}
 
-type role struct {
-	name                 string
-	roleDependencies     map[string]role
-	schemasWithOwnership map[string]struct{}
-}
-
-type roleOption func(r *role)
-
 var (
 	// PostgreSQL provides a set of predefined roles that provide access to
 	// certain, commonly needed, privileged capabilities and information
@@ -62,48 +54,6 @@ var (
 		"cloudsqlinactiveuser":           {},
 	}
 )
-
-func newRole(name string, opts ...roleOption) role {
-	r := role{
-		name:                 name,
-		schemasWithOwnership: make(map[string]struct{}),
-		roleDependencies:     make(map[string]role),
-	}
-	for _, opt := range opts {
-		opt(&r)
-	}
-	return r
-}
-
-func withOwner(schema ...string) roleOption {
-	return func(r *role) {
-		if len(schema) == 0 {
-			return
-		}
-		for _, s := range schema {
-			if s != "" {
-				r.schemasWithOwnership[s] = struct{}{}
-			}
-		}
-	}
-}
-
-func withRoleDeps(deps ...string) roleOption {
-	return func(r *role) {
-		if len(deps) == 0 {
-			return
-		}
-		for _, dep := range deps {
-			if dep != "" {
-				r.roleDependencies[dep] = newRole(dep)
-			}
-		}
-	}
-}
-
-func (r *role) isOwner() bool {
-	return len(r.schemasWithOwnership) > 0
-}
 
 func (p *roleSQLParser) extractRoleNamesFromDump(rolesDump []byte) map[string]role {
 	scanner := bufio.NewScanner(bytes.NewReader(rolesDump))
