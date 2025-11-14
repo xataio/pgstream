6567e937d20655851c76193a7d1f38898a9f944c
Arochka <admin@arochka.com>
2025-11-14 15:24:38 +0100

    Fix integration tests

--- migrations/postgres/9_add_index_replication_support.down.sql
+++ migrations/postgres/9_add_index_replication_support.down.sql
@@ -44,17 +44,25 @@ BEGIN
         END IF;
 
     ELSIF tg_event = 'ddl_command_end' THEN
-        IF tg_tag IN ('CREATE SCHEMA', 'CREATE TABLE', 'ALTER TABLE') THEN
-            SELECT schema_name INTO rec_schema_name FROM pg_event_trigger_ddl_commands()
-            WHERE object_type IN ('schema', 'table', 'table column')
-            AND command_tag = tg_tag LIMIT 1;
+        IF tg_tag = 'CREATE SCHEMA' THEN
+            SELECT object_identity INTO rec_schema_name
+            FROM pg_event_trigger_ddl_commands()
+            WHERE object_type = 'schema'
+              AND command_tag = 'CREATE SCHEMA'
+            LIMIT 1;
+        ELSIF tg_tag IN ('CREATE TABLE', 'ALTER TABLE') THEN
+            SELECT schema_name INTO rec_schema_name
+            FROM pg_event_trigger_ddl_commands()
+            WHERE object_type IN ('table', 'table column')
+              AND command_tag = tg_tag
+            LIMIT 1;
+        END IF;
 
-            is_system_schema := pgstream.is_system_schema(rec_schema_name);
-            IF rec_schema_name IS NOT NULL AND NOT is_system_schema THEN
-                SELECT COALESCE((SELECT version+1 FROM "pgstream"."schema_log" WHERE schema_name = rec_schema_name ORDER BY version DESC LIMIT 1), 1) INTO schema_version;
-                INSERT INTO "pgstream"."schema_log" (version, schema_name, schema)
-                VALUES (schema_version, rec_schema_name, pgstream.get_schema(rec_schema_name));
-            END IF;
+        is_system_schema := pgstream.is_system_schema(rec_schema_name);
+        IF rec_schema_name IS NOT NULL AND NOT is_system_schema THEN
+            SELECT COALESCE((SELECT version+1 FROM "pgstream"."schema_log" WHERE schema_name = rec_schema_name ORDER BY version DESC LIMIT 1), 1) INTO schema_version;
+            INSERT INTO "pgstream"."schema_log" (version, schema_name, schema)
+            VALUES (schema_version, rec_schema_name, pgstream.get_schema(rec_schema_name));
         END IF;
     END IF;
 END;
