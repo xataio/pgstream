aa2ce35d5dccbf3d6ce21efdc2e0757bb7715e2a
eminano <esther@xata.io>
2025-11-18 14:03:07 +0100

    Update progress bar lib

--- internal/progress/progress_bar.go
+++ internal/progress/progress_bar.go
@@ -11,6 +11,8 @@ import (
 type Bar interface {
 	Add(int) error
 	Add64(int64) error
+	Current() int64
+	SetCurrent(int64) error
 	Close() error
 }
 
@@ -18,9 +20,37 @@ type ProgressBar struct {
 	*progressbar.ProgressBar
 }
 
-func NewBar(max int, opts ...progressbar.Option) *ProgressBar {
+func (pb *ProgressBar) Current() int64 {
+	return pb.ProgressBar.State().CurrentNum
+}
+
+func (pb *ProgressBar) SetCurrent(value int64) error {
+	return pb.Set64(value)
+}
+
+func NewBar(total int64, description, unit string) *ProgressBar {
 	return &ProgressBar{
-		ProgressBar: progressbar.NewOptions(max, opts...),
+		ProgressBar: progressbar.NewOptions64(total,
+			progressbar.OptionShowCount(),
+			progressbar.OptionShowIts(),
+			progressbar.OptionSetItsString(unit),
+			progressbar.OptionSetRenderBlankState(true),
+			progressbar.OptionSetWidth(20),
+			progressbar.OptionSetPredictTime(true),
+			progressbar.OptionEnableColorCodes(true),
+			progressbar.OptionShowElapsedTimeOnFinish(),
+			progressbar.OptionSetDescription(description),
+			progressbar.OptionOnCompletion(func() {
+				fmt.Printf("\n") //nolint:forbidigo
+			}),
+			progressbar.OptionSetTheme(progressbar.Theme{
+				Saucer:        "[green]=[reset]",
+				SaucerHead:    "[green]>[reset]",
+				SaucerPadding: " ",
+				BarStart:      "[",
+				BarEnd:        "]",
+			}),
+		),
 	}
 }
 
@@ -31,7 +61,6 @@ func NewBytesBar(totalBytes int64, description string) *ProgressBar {
 			progressbar.OptionSetRenderBlankState(true),
 			progressbar.OptionSetPredictTime(true),
 			progressbar.OptionSetWidth(20),
-			progressbar.OptionSetPredictTime(true),
 			progressbar.OptionEnableColorCodes(true),
 			progressbar.OptionShowBytes(true),
 			progressbar.OptionShowTotalBytes(true),
@@ -46,6 +75,7 @@ func NewBytesBar(totalBytes int64, description string) *ProgressBar {
 				SaucerPadding: " ",
 				BarStart:      "[",
 				BarEnd:        "]",
-			})),
+			}),
+		),
 	}
 }
