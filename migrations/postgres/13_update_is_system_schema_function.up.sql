534e972981e9195fb14bf1b108eefda44b79e3bc
eminano <esther@xata.io>
2026-01-26 17:31:47 +0100

    Remove old migrations

--- migrations/postgres/13_update_is_system_schema_function.up.sql
+++
@@ -1,6 +0,0 @@
--- Add pg_temp schema to system schemas list
-CREATE OR REPLACE FUNCTION pgstream.is_system_schema(schema_name text) RETURNS boolean AS $$
-BEGIN
-    RETURN schema_name IN ('pgstream', 'pgroll', 'pg_temp');
-END;
-$$ LANGUAGE plpgsql;
