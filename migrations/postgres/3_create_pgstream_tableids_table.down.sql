08b56d880393e3535ef0e93ec65e3e80289f0a1c
eminano <esther@xata.io>
2026-01-26 17:31:47 +0100

    Remove old migrations

--- migrations/postgres/3_create_pgstream_tableids_table.down.sql
+++
@@ -1,2 +0,0 @@
-DROP FUNCTION IF EXISTS pgstream.create_table_mapping;
-DROP TABLE IF EXISTS pgstream.table_ids;
--- migrations/postgres/7_create_pgstream_event_triggers.down.sql
+++
@@ -1,2 +0,0 @@
-DROP EVENT TRIGGER IF EXISTS pgstream_log_schema_create_alter_table;
-DROP EVENT TRIGGER IF EXISTS pgstream_log_schema_drop_schema_table;
