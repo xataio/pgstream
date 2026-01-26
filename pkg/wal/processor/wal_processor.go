52d140c9a82eb8a598d715d2ce62ac4a0e82e86d
eminano <esther@xata.io>
2026-01-26 16:59:33 +0100

    Remove schemalog from processor pkg

--- pkg/wal/processor/wal_processor.go
+++ pkg/wal/processor/wal_processor.go
@@ -4,11 +4,8 @@ package processor
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
-	"fmt"
 
-	"github.com/xataio/pgstream/pkg/schemalog"
 	"github.com/xataio/pgstream/pkg/wal"
 )
 
@@ -19,39 +16,4 @@ type Processor interface {
 	Name() string
 }
 
-var (
-	ErrPanic               = errors.New("panic while processing wal event")
-	ErrIncompatibleWalData = errors.New("wal data event is not a schema log entry")
-)
-
-// IsSchemaLogEvent will return true if the wal event data originates from the
-// pgstream schema and the pgstream schema_log table.
-func IsSchemaLogEvent(d *wal.Data) bool {
-	return d.Schema == schemalog.SchemaName && d.Table == schemalog.TableName
-}
-
-// WalDataToLogEntry will convert the wal event data on input into the
-// equivalent schemalog entry. It will return an error if the wal event data is
-// not from the schema log table.
-func WalDataToLogEntry(d *wal.Data) (*schemalog.LogEntry, error) {
-	if !IsSchemaLogEvent(d) {
-		return nil, ErrIncompatibleWalData
-	}
-
-	intermediateRec := make(map[string]any, len(d.Columns))
-	for _, col := range d.Columns { // we only process inserts, so identity columns should never be set
-		intermediateRec[col.Name] = col.Value
-	}
-
-	intermediateRecBytes, err := json.Marshal(intermediateRec)
-	if err != nil {
-		return nil, fmt.Errorf("parsing wal event into schema log entry, intermediate record is not valid JSON: %w", err)
-	}
-
-	var le schemalog.LogEntry
-	if err := json.Unmarshal(intermediateRecBytes, &le); err != nil {
-		return nil, fmt.Errorf("parsing wal event into schema, intermediate record is not valid JSON: %w", err)
-	}
-
-	return &le, nil
-}
+var ErrPanic = errors.New("panic while processing wal event")
