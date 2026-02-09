00b193d12aee631ef7c97b606cbc61ec9663244b
eminano <esther@xata.io>
2026-02-09 11:57:24 +0100

    Quote schema names during schema snapshot

--- pkg/snapshot/generator/postgres/schema/pgdumprestore/snapshot_pg_dump_restore_generator.go
+++ pkg/snapshot/generator/postgres/schema/pgdumprestore/snapshot_pg_dump_restore_generator.go
@@ -168,7 +168,7 @@ func (s *SnapshotGenerator) CreateSnapshot(ctx context.Context, ss *snapshot.Sna
 	dumpSchemas := make(map[string][]string, len(ss.SchemaTables))
 	for schema, tables := range ss.SchemaTables {
 		if len(tables) > 0 {
-			dumpSchemas[schema] = tables
+			dumpSchemas[quoteSchema(schema)] = tables
 		}
 	}
 	// nothing to dump
@@ -716,4 +716,11 @@ func extractEventTriggerSchema(line string) (string, error) {
 	}
 
 	return pglib.QuoteIdentifier(functionParts[0]), nil
+}
+
+func quoteSchema(schema string) string {
+	if schema == wildcard {
+		return wildcard
+	}
+	return pglib.QuoteIdentifier(schema)
 }
